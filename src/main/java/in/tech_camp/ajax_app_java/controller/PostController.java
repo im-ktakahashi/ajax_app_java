00e0437e4dcbb5826e398356c2598fa9e0650825
高橋洸允 <163371042+im-ktakahashi@users.noreply.github.com>
2025-01-24 13:42:18 +0900

    リクエストを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -1,5 +1,6 @@
 package in.tech_camp.ajax_app_java.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,27 +16,27 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class PostController {
 
-  private final PostRepository postRepository;
+    private final PostRepository postRepository;
 
-  @GetMapping("/")
-  public String showList(Model model) {
-    var postList = postRepository.findAll();
-    model.addAttribute("postList", postList);
-    model.addAttribute("postForm", new PostForm());
-    return "posts/index";
-  }
+    @GetMapping("/")
+    public String showList(Model model) {
+        var postList = postRepository.findAll();
+        model.addAttribute("postList", postList);
+        model.addAttribute("postForm", new PostForm());
+        return "posts/index";
+    }
 
-  // @GetMapping("/postForm")
-  // public String showPostForm(@ModelAttribute("postForm") PostForm form){
-  //     return "posts/postForm";
-  // }
-
-  @PostMapping("/posts")
-  public String savePost(@ModelAttribute("postForm") PostForm form){
-    PostEntity post = new PostEntity();
-    post.setContent(form.getContent());
-    postRepository.insert(post);
-    return "redirect:/";
-  }
+    // @GetMapping("/postForm")
+    // public String showPostForm(@ModelAttribute("postForm") PostForm form){
+    //     return "posts/postForm";
+    // }
+    @PostMapping("/posts")
+    public ResponseEntity<PostEntity> savePost(@ModelAttribute("postForm") PostForm form) {
+        PostEntity post = new PostEntity();
+        post.setContent(form.getContent());
+        postRepository.insert(post);
+        PostEntity resultPost = postRepository.findById(post.getId());
+        return ResponseEntity.ok(resultPost);
+    }
 
 }
